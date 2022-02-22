36d384fead6fb2415b444e911ab4abb8fa252f32
Ekey <infinirt@hotmail.com>
2022-02-22 21:45:21 +0300

    Main

--- KUF2.Unpacker/KUF2.Unpacker/FileSystem/Encryption/PkgCipher.cs
+++ KUF2.Unpacker/KUF2.Unpacker/FileSystem/Encryption/PkgCipher.cs
@@ -6,23 +6,20 @@ namespace KUF2.Unpacker
     {
         public static UInt32 iDecryptUInt32(UInt32 dwValue, PkgHeader m_Header)
         {
-            UInt32 dwResult = m_Header.dwSeed * 0x19660D + 0x3C6EF35F;
-            m_Header.dwSeed = dwResult;
-            return dwValue + dwResult;
+            m_Header.dwSeed = m_Header.dwSeed * 0x19660D + 0x3C6EF35F;
+            return dwValue + m_Header.dwSeed;
         }
 
         public static Int32 iDecryptInt32(Int32 dwValue, PkgHeader m_Header)
         {
-            UInt32 dwResult = m_Header.dwSeed * 0x19660D + 0x3C6EF35F;
-            m_Header.dwSeed = dwResult;
-            return (Int32)(dwValue + dwResult);
+            m_Header.dwSeed = m_Header.dwSeed * 0x19660D + 0x3C6EF35F;
+            return (Int32)(dwValue + m_Header.dwSeed);
         }
 
         public static UInt64 iDecryptUInt64(UInt64 dwValue, PkgHeader m_Header)
         {
-            UInt32 dwResult = m_Header.dwSeed * 0x19660D + 0x3C6EF35F;
-            m_Header.dwSeed = dwResult;
-            return dwValue + dwResult;
+            m_Header.dwSeed = m_Header.dwSeed * 0x19660D + 0x3C6EF35F;
+            return dwValue + m_Header.dwSeed;
         }
     }
 }
